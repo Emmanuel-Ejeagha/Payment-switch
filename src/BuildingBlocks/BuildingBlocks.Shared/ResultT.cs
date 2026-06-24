8c9fc77f4fd26945079b09e41eb72c327b53d311
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-24 16:41:59 +0100

    feat(building-blocks): add Result, Result<T>, and Error types

---
+++ src/BuildingBlocks/BuildingBlocks.Shared/ResultT.cs
@@ -0,0 +1,22 @@
+﻿namespace BuildingBlocks.Shared;
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(T? value, bool isSuccess, IReadOnlyList<Error> errors) : base(errors)
+    {
+        Value = value;
+    }
+
+    public static Result<T> Success(T value) => new(value, true, new List<Error>());
+    public new static Result<T> Failure(IEnumerable<Error> errors) => new(default, false, errors.ToList());
+    public new static Result<T> Failure(params Error[] errors) => new(default, false, errors.ToList());
+
+    public static implicit operator Result<T>(T value) => Success(value);
+    public static implicit operator Result<T>(Error error) => Failure(error);
+    public static implicit operator Result<T>(List<Error> errors) => Failure(errors);
+    public static implicit operator Result<T>(Error[] errors) => Failure(errors);
+
+
+}
