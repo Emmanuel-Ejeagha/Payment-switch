7ff68022a39dcaf57487033e5f71c91da5a95625
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-24 22:50:24 +0100

    feat(building-blocks): add domain event dispatcher interfaces and implementation

---
+++ src/BuildingBlocks/BuildingBlocks.Shared/IDomainEventDispatcher.cs
@@ -0,0 +1,6 @@
+﻿namespace BuildingBlocks.Shared;
+
+public interface IDomainEventDispatcher
+{
+    Task DispatchAsync(IReadOnlyList<DomainEvent> domainEvents, CancellationToken cancellationToken = default);
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Shared/IDomainEventHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace BuildingBlocks.Shared;
+
+public interface IDomainEventHandler<TEvent> where TEvent : DomainEvent
+{
+    Task Handle(TEvent domainEvent, CancellationToken cancellationToken = default);
+}
