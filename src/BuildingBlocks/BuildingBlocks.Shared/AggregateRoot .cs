1af629ac7a41e790b3da770e9a247593b8d4a221
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-24 17:58:53 +0100

    feat(building-blocks): add BaseEntity, AggregateRoot, and minimal DomainEvent

---
+++ src/BuildingBlocks/BuildingBlocks.Shared/AggregateRoot .cs
@@ -0,0 +1,22 @@
+﻿namespace BuildingBlocks.Shared;
+
+public abstract class AggregateRoot : BaseEntity
+{
+    private readonly List<DomainEvent> _domainEvents = new();
+
+    public IReadOnlyList<DomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected AggregateRoot() : base() { }
+
+    protected AggregateRoot(Guid id) : base(id) { }
+}
