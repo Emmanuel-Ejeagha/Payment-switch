5845b9e323e8a54f0bc23c1ed8f83c92664e12eb
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-26 15:50:13 +0100

    feat(identity): add Jwt, PassWordHasher, Token and UnitOfWork services

---
+++ src/Services/Identity/Identity.Infrastructure/Services/UnitOfWork .cs
@@ -0,0 +1,18 @@
+﻿using Identity.Application.Interfaces;
+
+namespace Identity.Infrastructure.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+}
