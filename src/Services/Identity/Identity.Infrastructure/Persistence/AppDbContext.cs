0f00c066b61fa49318ad698bc48a396236c46a75
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-26 15:53:03 +0100

    feat(identity): add AppDbContext and ContextFactory

---
+++ src/Services/Identity/Identity.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Identity.Domain.Entities;
+using Identity.Infrastructure.Outbox;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<OutboxMessage> OutboxMessages { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
