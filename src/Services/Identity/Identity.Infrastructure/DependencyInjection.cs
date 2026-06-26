6768e9a7e62a505cd9a3885486c552a4a59d4df1
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-26 15:53:58 +0100

    feat(identity): add services to DI container

---
+++ src/Services/Identity/Identity.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using Identity.Application.Interfaces;
+using Identity.Infrastructure.Outbox;
+using Identity.Infrastructure.Persistence;
+using Identity.Infrastructure.Persistence.Repositories;
+using Identity.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Identity.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddIdentityInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<OutboxInterceptor>();
+        services.AddDbContext<AppDbContext>((sp, options) =>
+        {
+            var interceptor = sp.GetRequiredService<OutboxInterceptor>();
+            options.UseNpgsql(configuration.GetConnectionString("IdentityDb"))
+                   .AddInterceptors(interceptor);
+        });
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+        services.AddScoped<ITokenService, TokenService>();
+        services.Configure<JwtSettings>(configuration.GetSection("Jwt"));
+
+        return services;
+    }
+}
