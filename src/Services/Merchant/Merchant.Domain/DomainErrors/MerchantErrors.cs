4975b194d7e53202a6da0ad61edc3034699f535e
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-27 17:28:10 +0100

    feat(merchant.domain): add domain layer with Merchant errors

---
+++ src/Services/Merchant/Merchant.Domain/DomainErrors/MerchantErrors.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Shared.Results;
+
+namespace Merchant.Domain.DomainErrors;
+
+public static class MerchantErrors
+{
+    public static Error MerchantNotFound(Guid merchantId) =>
+        new("Merchant.MerchantNotFound", $"Merchant with Id '{merchantId}' not found.");
+
+    public static Error AlreadyOnboarded(Guid merchantId) =>
+        new("Merchant.AlreadyOnboarded", $"Merchant with Id '{merchantId}' is already onboarded.");
+
+    public static Error InvalidStatusTransition(string current, string target) =>
+        new("Merchant.InvalidStatusTransition", $"Cannot transition from '{current}' to '{target}'.");
+
+    public static Error InvalidEmailFormat(string email) =>
+        new("Merchant.InvalidEmailFormat", $"Email '{email}' has an invalid format.");
+
+    public static Error EmailAlreadyInUse(string email) =>
+        new("Merchant.EmailAlreadyInUse", $"Email '{email}' is already used by another merchant.");
+}
