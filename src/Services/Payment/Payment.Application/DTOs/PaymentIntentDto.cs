6dce60be9ddb930020dc6c46bd6ac9d38f0f019d
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-29 18:13:27 +0100

    feat(pament.application): add DTOs

---
+++ src/Services/Payment/Payment.Application/DTOs/PaymentIntentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Payment.Application.DTOs;
+
+public record PaymentIntentDto(
+    Guid IntentId,
+    Guid MerchantId,
+    decimal Amount,
+    string Currency,
+    string Status,
+    List<TransactionDto> Transactions
+);
---
+++ src/Services/Payment/Payment.Application/DTOs/PaymentIntentResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace Payment.Application.DTOs;
+
+public record PaymentIntentResponse(Guid IntentId, string Status, string? ClientSecret);
