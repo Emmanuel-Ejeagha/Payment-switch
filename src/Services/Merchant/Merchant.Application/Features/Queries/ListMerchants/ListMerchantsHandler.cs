3ea544a4bbce258be7329a341d2cd4450410244d
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-29 12:28:28 +0100

    feat(merchant.application): implement ListMerchant query and handler

---
+++ src/Services/Merchant/Merchant.Application/Features/Queries/ListMerchants/ListMerchantsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Merchant.Application.Features.Queries.ListMerchants;
+
+public class ListMerchantsHandler
+{
+    private readonly IMerchantRepository _repository;
+
+    public ListMerchantsHandler(IMerchantRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result<List<MerchantDto>>> Handle(ListMerchantsQuery query, CancellationToken cancellationToken = default)
+    {
+        var merchants = await _repository.ListAsync(query.Skip, query.Take, cancellationToken);
+        return merchants;
+    }
+}
