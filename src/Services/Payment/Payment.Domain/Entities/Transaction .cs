46721be8f69c2dd2fd0087f3d61a7459458c71b7
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-29 17:04:23 +0100

    feat(payment.domain): add PaymentIntent and Transaction entities

---
+++ src/Services/Payment/Payment.Domain/Entities/Transaction .cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.Shared.Aggregate;
+using Payment.Domain.Enums;
+using Payment.Domain.ValueObjects;
+
+namespace Payment.Domain.Entities;
+
+public class Transaction : BaseEntity
+{
+    public TransactionType Type { get; private set; }
+    public Money Amount { get; private set; } = default!;
+    public GatewayReference? GatewayReference { get; private set; }
+    public DateTime Timestamp { get; private set; }
+
+    private Transaction() : base() { }
+
+    public Transaction(TransactionType type, Money amount, GatewayReference? gatewayReference = null) : base()
+    {
+        Type = type;
+        Amount = amount ?? throw new ArgumentNullException(nameof(amount));
+        GatewayReference = gatewayReference;
+        Timestamp = DateTime.UtcNow;
+    }
+}
