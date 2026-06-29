b085006ea8e7d4172d1147785db6baed3e4f071a
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-29 12:30:50 +0100

    feat(merchant.application): feat(merchant.application):

---
+++ src/Services/Merchant/Merchant.Application/Global.cs
@@ -0,0 +1,8 @@
+﻿global using FluentValidation;
+global using Merchant.Application.DTOs;
+global using Merchant.Application.Interfaces;
+global using Merchant.Domain.DomainErrors;
+global using Merchant.Domain.ValueObjects;
+global using MerchantEntity = Merchant.Domain.Entities.Merchant;
+global using BuildingBlocks.Shared.Events;
+global using BuildingBlocks.Shared.Results;
