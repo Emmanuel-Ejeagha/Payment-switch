e9f3dcdb774580075a38abd263545b77f3885a97
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-06-29 18:20:40 +0100

    feat(payment.application): implement ListPaymentIntentsByMerchant query, and handler

---
+++ src/Services/Payment/Payment.Application/Features/Queries/ListPaymentIntentsByMerchant/ListPaymentIntentsByMerchantHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Shared.Results;
+using Payment.Application.DTOs;
+using Payment.Application.Interfaces;
+
+namespace Payment.Application.Features.Queries.ListPaymentIntentsByMerchant;
+
+public class ListPaymentIntentsByMerchantHandler
+{
+    private readonly IPaymentIntentRepository _repository;
+
+    public ListPaymentIntentsByMerchantHandler(IPaymentIntentRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Result<List<PaymentIntentDto>>> Handle(ListPaymentIntentsByMerchantQuery query, CancellationToken cancellationToken = default)
+    {
+        var intents = await _repository.ListByMerchantAsync(query.MerchantId, query.Skip, query.Take, cancellationToken);
+        return intents;
+    }
+}
---
+++ src/Services/Payment/Payment.Application/Features/Queries/ListPaymentIntentsByMerchant/ListPaymentIntentsByMerchantQuery.cs
@@ -0,0 +1,3 @@
+﻿namespace Payment.Application.Features.Queries.ListPaymentIntentsByMerchant;
+
+public record ListPaymentIntentsByMerchantQuery(Guid MerchantId, int Skip, int Take);
