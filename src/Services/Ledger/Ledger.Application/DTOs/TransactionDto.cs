e9415ee0f952589d0c180d83a561557fe398b587
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-01 16:55:05 +0100

    feat(ledger.application): add balance and transaction dto

---
+++ src/Services/Ledger/Ledger.Application/DTOs/TransactionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Ledger.Application.DTOs;
+
+public record TransactionDto(
+    Guid Id,
+    string Type,
+    decimal Amount,
+    string Currency,
+    string Description,
+    DateTime Timestamp
+);
