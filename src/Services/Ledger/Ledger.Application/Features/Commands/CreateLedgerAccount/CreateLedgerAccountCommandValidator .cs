5673a62e360df93c1d004507a80b99dc782ae971
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-01 16:57:19 +0100

    feat(ledger.application): implement CreateLedgerAccount command, validator and handler

---
+++ src/Services/Ledger/Ledger.Application/Features/Commands/CreateLedgerAccount/CreateLedgerAccountCommandValidator .cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Ledger.Application.Features.Commands.CreateLedgerAccount;
+
+public class CreateLedgerAccountCommandValidator : AbstractValidator<CreateLedgerAccountCommand>
+{
+    public CreateLedgerAccountCommandValidator()
+    {
+        RuleFor(x => x.MerchantId).NotEmpty();
+        RuleFor(x => x.Currency).NotEmpty().Length(3).WithMessage("Currency must be a 3-letter ISO code.");
+    }
+}
