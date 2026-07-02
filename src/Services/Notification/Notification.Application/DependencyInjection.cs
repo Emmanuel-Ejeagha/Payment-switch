802111130a01a084e967898b9d21f48477dc0fa6
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-02 16:03:11 +0100

    feat(notification.application): add  handlers to DI

---
+++ src/Services/Notification/Notification.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.Shared.Events;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Notification.Application.Features.Commands.CreateNotification;
+using Notification.Application.Features.Commands.SendPendingNotification;
+using Notification.Application.Features.Queries.GetNotificationById;
+using Notification.Application.Features.Queries.ListNotifications;
+
+namespace Notification.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddNotificationApplication(this IServiceCollection services)
+    {
+        services.AddScoped<CreateNotificationHandler>();
+        services.AddScoped<SendPendingNotificationHandler>();
+        services.AddScoped<GetNotificationByIdHandler>();
+        services.AddScoped<ListNotificationsHandler>();
+
+        services.AddScoped<IDomainEventDispatcher, DomainEventDispatcher>();
+
+        services.AddValidatorsFromAssemblyContaining<CreateNotificationCommandValidator>();
+
+        return services;
+    }
+}
