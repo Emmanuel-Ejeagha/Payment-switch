676a1c388f3d77030a4aab3ec78c1d1e9fab78df
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-02 14:50:34 +0100

    feat(notication.domain): add domain events

---
+++ src/Services/Notification/Notification.Domain/DomainEvents/NotificationSentEvent.cs
@@ -0,0 +1,5 @@
+﻿using BuildingBlocks.Shared.Events;
+
+namespace Notification.Domain.DomainEvents;
+
+public record NotificationSentEvent(Guid NotificationId, string Recipient, string Channel) : DomainEvent;
