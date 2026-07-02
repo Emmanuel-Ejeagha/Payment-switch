c56be1ee77c8cc8bbea4d883cfa1697524575c8a
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-02 16:01:35 +0100

    feat(notification.application): add interfaces

---
+++ src/Services/Notification/Notification.Application/Interfaces/INotificationSender.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Shared.Results;
+using NotificationEntity = Notification.Domain.Entities.Notification;
+
+namespace Notification.Application.Interfaces;
+
+public interface INotificationSender
+{
+    Task<Result> SendAsync(NotificationEntity notification, CancellationToken cancellationToken = default);
+}
