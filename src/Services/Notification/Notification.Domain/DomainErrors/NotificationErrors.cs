11b55801301e8fbeb1c5fdef89bd432b5615dec5
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-02 14:49:31 +0100

    feat(notification.domain): add domain errors

---
+++ src/Services/Notification/Notification.Domain/DomainErrors/NotificationErrors.cs
@@ -0,0 +1,13 @@
+﻿
+using BuildingBlocks.Shared.Results;
+
+namespace Notification.Domain.DomainErrors;
+
+public static class NotificationErrors
+{
+    public static Error InvalidRecipient =>
+        new("Notification.InvalidRecipient", "Recipient cannot be empty.");
+
+    public static Error InvalidChannel =>
+        new("Notification.InvalidChannel", "Invalid notification channel.");
+}
