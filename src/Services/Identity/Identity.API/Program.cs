299b620e6fb5a8dc5dead41b57db21dc9c442201
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-06 13:53:05 +0100

    hotfix: add Swagger base path for Nginx routing

--- src/Services/Identity/Identity.API/Program.cs
+++ src/Services/Identity/Identity.API/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.AddServer(new OpenApiServer { Url = "/identity" });
     c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity API", Version = "v1" });
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
