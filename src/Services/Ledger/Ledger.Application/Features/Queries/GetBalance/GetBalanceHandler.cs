76de400bc0f7ffc8962d135f4ccfac1d8dcfd359
Emmanuel Ejeagha <emma.ejeagha@gmail.com>
2026-07-27 12:20:57 +0100

    Auto-create ledger account in GetBalance if not found

--- src/Services/Ledger/Ledger.Application/Features/Queries/GetBalance/GetBalanceHandler.cs
+++ src/Services/Ledger/Ledger.Application/Features/Queries/GetBalance/GetBalanceHandler.cs
@@ -2,6 +2,7 @@
 using Ledger.Application.DTOs;
 using Ledger.Application.Interfaces;
 using Ledger.Domain.DomainErrors;
+using Ledger.Domain.Entities;
 using Microsoft.Extensions.Logging;
 
 namespace Ledger.Application.Features.Queries.GetBalance;
@@ -9,11 +10,13 @@ namespace Ledger.Application.Features.Queries.GetBalance;
 public class GetBalanceHandler
 {
     private readonly ILedgerAccountRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
     private readonly ILogger<GetBalanceHandler> _logger;
 
-    public GetBalanceHandler(ILedgerAccountRepository repository, ILogger<GetBalanceHandler> logger)
+    public GetBalanceHandler(ILedgerAccountRepository repository, IUnitOfWork unitOfWork, ILogger<GetBalanceHandler> logger)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
         _logger = logger;
     }
 
@@ -22,7 +25,12 @@ public class GetBalanceHandler
         _logger.LogInformation("Handling {CommandName} for Merchant {MerchantId}", nameof(GetBalanceQuery), query.MerchantId);
         var account = await _repository.GetByMerchantIdAsync(query.MerchantId, cancellationToken);
         if (account is null)
-            return LedgerErrors.AccountNotFound(query.MerchantId);
+        {
+            _logger.LogInformation("No ledger account found for merchant {MerchantId}; auto-creating", query.MerchantId);
+            account = new LedgerAccount(Guid.NewGuid(), query.MerchantId, "NGN");
+            await _repository.AddAsync(account, cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
 
         return new BalanceDto(account.MerchantId, account.AvailableBalance, account.PendingBalance, account.ReservedBalance, account.Currency);
     }
